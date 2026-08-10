131d942bb401e5d216dfe099d413fe42aa681a87
Muhammad Fahad Saleem <mfahadsaleem@gmail.com>
2026-08-10 10:40:30 +0500

    Fix Vercel production database configuration

--- run.py
+++ run.py
@@ -2,8 +2,12 @@ import os
 from app import create_app
 from app.extensions import db
 
-app = create_app(os.environ.get('FLASK_ENV', 'development'))
-
+app = create_app(
+    os.environ.get(
+        'FLASK_ENV',
+        'production' if os.environ.get('VERCEL') == '1' else 'development'
+    )
+)
 
 @app.shell_context_processor
 def make_shell_context():
